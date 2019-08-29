25036a339c48cb2488043f072d42baa1a993e4f7
Karl F. Meinkopf <kremen.karl@yandex.ru>
2019-08-29 16:45:15 +0300

    week2 update

--- week2/ex1.c
+++ week2/ex1.c
@@ -8,15 +8,15 @@
 int main() {
     int32_t intmax = INT32_MAX;
     int32_t intmin = INT32_MIN;
-    float sfpmin = FLT_MIN;
+    float sfpmin = -FLT_MAX;
     float sfpmax = FLT_MAX;
-    double dfpmin = DBL_MIN;
+    double dfpmin = -DBL_MAX;
     double dfpmax = DBL_MAX;
 
-    PRINT_VAR("%d", intmax);
-    PRINT_VAR("%d", intmin);
-    PRINT_VAR("%f", sfpmax);
-    PRINT_VAR("%f", sfpmin);
-    PRINT_VAR("%f", dfpmax);
-    PRINT_VAR("%f", dfpmin);
+    PRINT_VAR("MAX %d", intmax);
+    PRINT_VAR("MIN %d", intmin);
+    PRINT_VAR("MAX %F", sfpmax);
+    PRINT_VAR("MIN %F", sfpmin);
+    PRINT_VAR("MAX %F", dfpmax);
+    PRINT_VAR("MIN %F", dfpmin);
 }
--- week2/ex2.c
+++ week2/ex2.c
@@ -1,25 +1,27 @@
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stddef.h>
 
 const char *strrev(char *input) {
-    size_t inplen = strlen(input);
+	size_t inplen = strlen(input);
 
-    char *result = malloc((inplen + 1) * sizeof(char));
-    
-    size_t pos = 0;
-    for(int i = inplen - 1; i >= 0; i--) {
-        result[pos++] = input[i];
-    }
-    result[pos] = '\0';
+	char *result = malloc((inplen + 1) * sizeof(char));
 
-    return result;
+	size_t pos = 0;
+	for (int i = inplen - 1; i >= 0; i--) {
+		result[pos++] = input[i];
+	}
+	result[pos] = '\0';
+
+	return result;
 }
 
 int main() {
-    char buf[255];
-    fscanf(stdin, "%s", buf);
-    const char *result = strrev(buf);
-    fprintf(stdout, "%s\n", result);
+    size_t size = 0;
+    char *buf = NULL;
+	size = getline(&buf, &size, stdin);
+
+	const char *result = strrev(buf);
+	fprintf(stdout, "%s\n", result);
 }
